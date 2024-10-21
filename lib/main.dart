d880a552cae1c3c247cd294a08a0cc282e9c951d
Suva0303 <kairat.alibekov.2004@mail.ru>
2024-10-21 17:44:43 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
 import 'book.dart';
+import 'favorites.dart';
+import 'profile.dart';
+import 'cart.dart'; // Импортируем новый экран
 
 void main() {
   runApp(BooksApp());
 }
 
-class BooksApp extends StatelessWidget {
+class BooksApp extends StatefulWidget {
+  @override
+  _BooksAppState createState() => _BooksAppState();
+}
+
+class _BooksAppState extends State<BooksApp> {
+  int _selectedIndex = 0; // Индекс для навигационной панели
+  List<Book> cart = []; // Корзина для товаров
+  List<Book> favorites = [];
+
+  final List<Widget> _pages = []; // Инициализация позже
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Инициализируем страницы с данными корзины и избранных книг
+    _pages.addAll([
+      BooksList(
+          onAddToCart: _addToCart), // Передаем функцию добавления в корзину
+      FavoritesScreen(favorites: favorites),
+      CartScreen(cartItems: cart),
+      ProfileScreen(),
+    ]);
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _addToCart(Book book) {
+    setState(() {
+      cart.add(book);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,22 +53,54 @@ class BooksApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         scaffoldBackgroundColor: const Color.fromARGB(255, 240, 230, 250),
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: Colors.black),
-          bodyMedium: TextStyle(color: Colors.black),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book,
+                  color: Color.fromARGB(255, 0, 0, 0)), // Белая иконка
+              label: 'Книги',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite,
+                  color: Color.fromARGB(255, 0, 0, 0)), // Белая иконка
+              label: 'Избранные',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart,
+                  color: Color.fromARGB(255, 0, 0, 0)), // Белая иконка
+              label: 'Корзина',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person,
+                  color: Color.fromARGB(255, 0, 0, 0)), // Белая иконка
+              label: 'Профиль',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.purple,
+          unselectedItemColor: Colors.white, // Цвет для невыбранных элементов
+          backgroundColor: Colors.purple, // Цвет фона навигационной панели
+          onTap: _onItemTapped,
         ),
       ),
-      home: BooksList(),
     );
   }
 }
 
 class BooksList extends StatelessWidget {
+  final Function(Book) onAddToCart;
+
+  BooksList({required this.onAddToCart});
+
   final List<Book> books = [
     Book(
       name: 'Оно',
       imageUrl: '../lib/images/it.jpeg',
-      price: 900, // Цены в рублях
+      price: 900,
     ),
     Book(
       name: 'Гарри Поттер',
@@ -53,100 +125,29 @@ class BooksList extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Доступные Книги'),
       ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: books.length,
-          itemBuilder: (context, index) {
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-                side: const BorderSide(color: Colors.black, width: 1),
-              ),
-              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: ListTile(
-                title: Text(books[index].name),
-                subtitle:
-                    Text('${books[index].price} ₽'), // Формат цен в рублях
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BookDetail(book: books[index]),
-                    ),
-                  );
-                },
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class BookDetail extends StatelessWidget {
-  final Book book;
-
-  BookDetail({required this.book});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(book.name),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.network(book.imageUrl),
-              const SizedBox(height: 8),
-              Text(book.name,
-                  style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black)),
-              const SizedBox(height: 16),
-              Text('${book.price} ₽',
-                  style: const TextStyle(
-                      fontSize: 20, color: Color.fromARGB(255, 13, 14, 13))),
-              const SizedBox(height: 20),
-              ElevatedButton(
+      body: ListView.builder(
+        itemCount: books.length,
+        itemBuilder: (context, index) {
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              side: const BorderSide(color: Colors.black, width: 1),
+            ),
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: ListTile(
+              title: Text(books[index].name),
+              subtitle: Text('${books[index].price} ₽'),
+              leading: Image.asset(books[index].imageUrl),
+              trailing: IconButton(
+                icon: const Icon(Icons.add_shopping_cart,
+                    color: Color.fromARGB(255, 0, 0, 0)), // Белая иконка
                 onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: Text('Купить ${book.name}',
-                          style: const TextStyle(color: Colors.black)),
-                      content: const Text('Вы желаете купить эту книгу?',
-                          style: TextStyle(color: Colors.black)),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Да',
-                              style: TextStyle(color: Colors.black)),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Нет',
-                              style: TextStyle(color: Colors.black)),
-                        ),
-                      ],
-                    ),
-                  );
+                  onAddToCart(books[index]);
                 },
-                child: const Text('Купить'),
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
